5abaaf5f714ddc5edbd7d74f7f2adc1470d6edf8
Rexmaster0 <97900485+Rexmaster0@users.noreply.github.com>
2024-06-10 20:34:08 +0300

    Add files via upload

---
+++ Dap.sql
@@ -0,0 +1,632 @@
+--
+-- Файл сгенерирован с помощью SQLiteStudio v3.4.1 в Пн июн 10 20:33:02 2024
+--
+-- Использованная кодировка текста: UTF-8
+--
+PRAGMA foreign_keys = off;
+BEGIN TRANSACTION;
+
+-- Таблица: Data
+CREATE TABLE IF NOT EXISTS Data (
+    num REAL,
+    id  INTEGER PRIMARY KEY AUTOINCREMENT
+);
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     499.648862,
+                     1
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     500.270163,
+                     2
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     500.321009,
+                     3
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     500.114357,
+                     4
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     500.36095,
+                     5
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     500.426395,
+                     6
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     500.849945,
+                     7
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     501.073061,
+                     8
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     500.713899,
+                     9
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     501.188913,
+                     10
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     500.835818,
+                     11
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     501.012484,
+                     12
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     500.829713,
+                     13
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     501.056044,
+                     14
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     500.271991,
+                     15
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     500.003826,
+                     16
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     500.540165,
+                     17
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     500.448606,
+                     18
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     500.437634,
+                     19
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     500.301146,
+                     20
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     500.427322,
+                     21
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     500.33744,
+                     22
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     501.007799,
+                     23
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     501.132615,
+                     24
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     501.492812,
+                     25
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     501.41782,
+                     26
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     501.409344,
+                     27
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     501.524715,
+                     28
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     501.302711,
+                     29
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     500.887753,
+                     30
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     500.862089,
+                     31
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     500.597619,
+                     32
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     500.686963,
+                     33
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     500.466965,
+                     34
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     500.338863,
+                     35
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     500.601773,
+                     36
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     500.542346,
+                     37
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     501.05335,
+                     38
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     501.091731,
+                     39
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     500.808274,
+                     40
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     500.828393,
+                     41
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     500.40972,
+                     42
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     500.503076,
+                     43
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     500.679534,
+                     44
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     500.518271,
+                     45
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     500.522391,
+                     46
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     500.478707,
+                     47
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     500.088393,
+                     48
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     500.017637,
+                     49
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     499.616603,
+                     50
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     500.405047,
+                     51
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     500.664254,
+                     52
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     501.317894,
+                     53
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     501.150928,
+                     54
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     501.615987,
+                     55
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     502.458432,
+                     56
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     502.540947,
+                     57
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     502.93777,
+                     58
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     503.23191,
+                     59
+                 );
+
+INSERT INTO Data (
+                     num,
+                     id
+                 )
+                 VALUES (
+                     503.055504,
+                     60
+                 );
+
+
+-- Таблица: Time
+CREATE TABLE IF NOT EXISTS Time (
+    time INTEGER
+);
+
+INSERT INTO Time (
+                     time
+                 )
+                 VALUES (
+                     1718038314
+                 );
+
+
+-- Таблица: Users
+CREATE TABLE IF NOT EXISTS Users (
+    id      INTEGER PRIMARY KEY AUTOINCREMENT
+                    UNIQUE,
+    user_id TEXT    UNIQUE,
+    dap     INTEGER DEFAULT (0),
+    bonus   INTEGER DEFAULT (0) 
+);
+
+INSERT INTO Users (
+                      id,
+                      user_id,
+                      dap,
+                      bonus
+                  )
+                  VALUES (
+                      1,
+                      'treasury',
+                      996499,
+                      0
+                  );
+
+INSERT INTO Users (
+                      id,
+                      user_id,
+                      dap,
+                      bonus
+                  )
+                  VALUES (
+                      2,
+                      'garik000000000',
+                      1600,
+                      10
+                  );
+
+INSERT INTO Users (
+                      id,
+                      user_id,
+                      dap,
+                      bonus
+                  )
+                  VALUES (
+                      3,
+                      'Dmitryuui',
+                      1600,
+                      10
+                  );
+
+INSERT INTO Users (
+                      id,
+                      user_id,
+                      dap,
+                      bonus
+                  )
+                  VALUES (
+                      4,
+                      'Whoiscalling_me',
+                      301,
+                      7
+                  );
+
+
+COMMIT TRANSACTION;
+PRAGMA foreign_keys = on;
